e5e4856c24b1d50010ac92645eda17ac42bbf323
杨骁文 <yangxw@yangxiaowendebijibendiannao.local>
2024-09-19 14:36:33 +0800

    rename all the exercise

--- ProofNet/Rudin.lean
+++ ProofNet/Rudin.lean
@@ -4,22 +4,22 @@ open Topology Filter Real Complex TopologicalSpace Finset
 open scoped BigOperators
 noncomputable section
 
-theorem exercise_1_1a
+theorem rudin_exercise_1_1a
   (x : ℝ) (y : ℚ) :
   ( Irrational x ) -> Irrational ( x + y ) :=
 sorry
 
-theorem exercise_1_1b
+theorem rudin_exercise_1_1b
 (x : ℝ)
 (y : ℚ)
 (h : y ≠ 0)
 : ( Irrational x ) -> Irrational ( x * y ) :=
 sorry
 
-theorem exercise_1_2 : ¬ ∃ (x : ℚ), ( x ^ 2 = 12 ) :=
+theorem rudin_exercise_1_2 : ¬ ∃ (x : ℚ), ( x ^ 2 = 12 ) :=
 sorry
 
-theorem exercise_1_4
+theorem rudin_exercise_1_4
 (α : Type*) [PartialOrder α]
 (s : Set α)
 (x y : α)
@@ -29,32 +29,32 @@ theorem exercise_1_4
 : x ≤ y :=
 sorry
 
-theorem exercise_1_5 (A minus_A : Set ℝ) (hA : A.Nonempty)
+theorem rudin_exercise_1_5 (A minus_A : Set ℝ) (hA : A.Nonempty)
   (hA_bdd_below : BddBelow A) (hminus_A : minus_A = {x | -x ∈ A}) :
   Inf A = Sup minus_A :=
 sorry
 
-theorem exercise_1_8 : ¬ ∃ (r : ℂ → ℂ → Prop), IsLinearOrder ℂ r :=
+theorem rudin_exercise_1_8 : ¬ ∃ (r : ℂ → ℂ → Prop), IsLinearOrder ℂ r :=
   sorry
 
-theorem exercise_1_11a (z : ℂ) :
+theorem rudin_exercise_1_11a (z : ℂ) :
   ∃ (r : ℝ) (w : ℂ), abs w = 1 ∧ z = r * w :=
 sorry
 
-theorem exercise_1_12 (n : ℕ) (f : ℕ → ℂ) :
+theorem rudin_exercise_1_12 (n : ℕ) (f : ℕ → ℂ) :
   abs (∑ i in range n, f i) ≤ ∑ i in range n, abs (f i) :=
 sorry
 
-theorem exercise_1_13 (x y : ℂ) :
+theorem rudin_exercise_1_13 (x y : ℂ) :
   |(abs x) - (abs y)| ≤ abs (x - y) :=
 sorry
 
-theorem exercise_1_14
+theorem rudin_exercise_1_14
   (z : ℂ) (h : abs z = 1)
   : (abs (1 + z)) ^ 2 + (abs (1 - z)) ^ 2 = 4 :=
 sorry
 
-theorem exercise_1_16a
+theorem rudin_exercise_1_16a
   (n : ℕ)
   (d r : ℝ)
   (x y z : EuclideanSpace ℝ (Fin n)) -- R^n
@@ -66,24 +66,24 @@ theorem exercise_1_16a
   : Set.Infinite {z : EuclideanSpace ℝ (Fin n) | ‖z - x‖ = r ∧ ‖z - y‖ = r} :=
 sorry
 
-theorem exercise_1_17
+theorem rudin_exercise_1_17
   (n : ℕ)
   (x y : EuclideanSpace ℝ (Fin n)) -- R^n
   : ‖x + y‖^2 + ‖x - y‖^2 = 2*‖x‖^2 + 2*‖y‖^2 :=
 sorry
 
-theorem exercise_1_18a
+theorem rudin_exercise_1_18a
   (n : ℕ)
   (h : n > 1)
   (x : EuclideanSpace ℝ (Fin n)) -- R^n
   : ∃ (y : EuclideanSpace ℝ (Fin n)), y ≠ 0 ∧ (inner x y) = (0 : ℝ) :=
 sorry
 
-theorem exercise_1_18b
+theorem rudin_exercise_1_18b
   : ¬ ∀ (x : ℝ), ∃ (y : ℝ), y ≠ 0 ∧ x * y = 0 :=
 sorry
 
-theorem exercise_1_19
+theorem rudin_exercise_1_19
   (n : ℕ)
   (a b c x : EuclideanSpace ℝ (Fin n))
   (r : ℝ)
@@ -93,52 +93,52 @@ theorem exercise_1_19
   : ‖x - a‖ = 2 * ‖x - b‖ ↔ ‖x - c‖ = r :=
 sorry
 
-theorem exercise_2_19a {X : Type*} [MetricSpace X]
+theorem rudin_exercise_2_19a {X : Type*} [MetricSpace X]
   (A B : Set X) (hA : IsClosed A) (hB : IsClosed B) (hAB : Disjoint A B) :
   SeparatedNhds A B :=
 sorry
 
-theorem exercise_2_24 {X : Type*} [MetricSpace X]
+theorem rudin_exercise_2_24 {X : Type*} [MetricSpace X]
   (hX : ∀ (A : Set X), Infinite A → ∃ (x : X), x ∈ closure A) :
   SeparableSpace X :=
 sorry
 
-theorem exercise_2_25 {K : Type*} [MetricSpace K] [CompactSpace K] :
+theorem rudin_exercise_2_25 {K : Type*} [MetricSpace K] [CompactSpace K] :
   ∃ (B : Set (Set K)), Set.Countable B ∧ IsTopologicalBasis B :=
 sorry
 
-theorem exercise_2_27a (k : ℕ) (E P : Set (EuclideanSpace ℝ (Fin k)))
+theorem rudin_exercise_2_27a (k : ℕ) (E P : Set (EuclideanSpace ℝ (Fin k)))
   (hE : E.Nonempty ∧ ¬ Set.Countable E)
   (hP : P = {x | ∀ U ∈ 𝓝 x, ¬ Set.Countable (P ∩ E)}) :
   IsClosed P ∧ P = {x | ClusterPt x (𝓟 P)}  :=
 sorry
 
-theorem exercise_2_27b (k : ℕ) (E P : Set (EuclideanSpace ℝ (Fin k)))
+theorem rudin_exercise_2_27b (k : ℕ) (E P : Set (EuclideanSpace ℝ (Fin k)))
   (hE : E.Nonempty ∧ ¬ Set.Countable E)
   (hP : P = {x | ∀ U ∈ 𝓝 x, (P ∩ E).Nonempty ∧ ¬ Set.Countable (P ∩ E)}) :
   Set.Countable (E \ P) :=
 sorry
 
-theorem exercise_2_28 (X : Type*) [MetricSpace X] [SeparableSpace X]
+theorem rudin_exercise_2_28 (X : Type*) [MetricSpace X] [SeparableSpace X]
   (A : Set X) (hA : IsClosed A) :
   ∃ P₁ P₂ : Set X, A = P₁ ∪ P₂ ∧
   IsClosed P₁ ∧ P₁ = {x | ClusterPt x (𝓟 P₁)} ∧
   Set.Countable P₂ :=
 sorry
 
-theorem exercise_2_29 (U : Set ℝ) (hU : IsOpen U) :
+theorem rudin_exercise_2_29 (U : Set ℝ) (hU : IsOpen U) :
   ∃ (f : ℕ → Set ℝ), (∀ n, ∃ a b : ℝ, f n = {x | a < x ∧ x < b}) ∧ (∀ n, f n ⊆ U) ∧
   (∀ n m, n ≠ m → f n ∩ f m = ∅) ∧
   U = ⋃ n, f n :=
 sorry
 
-theorem exercise_3_1a
+theorem rudin_exercise_3_1a
   (f : ℕ → ℝ)
   (h : ∃ (a : ℝ), Tendsto (λ (n : ℕ) => f n) atTop (𝓝 a))
   : ∃ (a : ℝ), Tendsto (λ (n : ℕ) => |f n|) atTop (𝓝 a) :=
 sorry
 
-theorem exercise_3_2a
+theorem rudin_exercise_3_2a
   : Tendsto (λ (n : ℝ) => (sqrt (n^2 + n) - n)) atTop (𝓝 (1/2)) :=
 sorry
 
@@ -146,11 +146,11 @@ noncomputable def f : ℕ → ℝ
 | 0 => sqrt 2
 | (n + 1) => sqrt (2 + sqrt (f n))
 
-theorem exercise_3_3
+theorem rudin_exercise_3_3
   : ∃ (x : ℝ), Tendsto f atTop (𝓝 x) ∧ ∀ n, f n < 2 :=
 sorry
 
-theorem exercise_3_5
+theorem rudin_exercise_3_5
   (a b : ℕ → ℝ)
   (h : limsup a + limsup b ≠ 0) :
   limsup (λ n => a n + b n) ≤ limsup a + limsup b :=
@@ -158,17 +158,17 @@ sorry
 
 def g (n : ℕ) : ℝ := sqrt (n + 1) - sqrt n
 
-theorem exercise_3_6a
+theorem rudin_exercise_3_6a
 : Tendsto (λ (n : ℕ) => (∑ i in range n, g i)) atTop atTop :=
 sorry
 
-theorem exercise_3_7
+theorem rudin_exercise_3_7
   (a : ℕ → ℝ)
   (h : ∃ y, (Tendsto (λ n => (∑ i in (range n), a i)) atTop (𝓝 y))) :
   ∃ y, Tendsto (λ n => (∑ i in (range n), sqrt (a i) / n)) atTop (𝓝 y) :=
 sorry
 
-theorem exercise_3_8
+theorem rudin_exercise_3_8
   (a b : ℕ → ℝ)
   (h1 : ∃ y, (Tendsto (λ n => (∑ i in (range n), a i)) atTop (𝓝 y)))
   (h2 : Monotone b)
@@ -176,7 +176,7 @@ theorem exercise_3_8
   ∃ y, Tendsto (λ n => (∑ i in (range n), (a i) * (b i))) atTop (𝓝 y) :=
 sorry
 
-theorem exercise_3_13
+theorem rudin_exercise_3_13
   (a b : ℕ → ℝ)
   (ha : ∃ y, (Tendsto (λ n => (∑ i in (range n), |a i|)) atTop (𝓝 y)))
   (hb : ∃ y, (Tendsto (λ n => (∑ i in (range n), |b i|)) atTop (𝓝 y))) :
@@ -184,14 +184,14 @@ theorem exercise_3_13
   λ i => (∑ j in range (i + 1), a j * b (i - j)))) atTop (𝓝 y)) :=
 sorry
 
-theorem exercise_3_20 {X : Type*} [MetricSpace X]
+theorem rudin_exercise_3_20 {X : Type*} [MetricSpace X]
   (p : ℕ → X) (l : ℕ) (r : X)
   (hp : CauchySeq p)
   (hpl : Tendsto (λ n => p (l * n)) atTop (𝓝 r)) :
   Tendsto p atTop (𝓝 r) :=
 sorry
 
-theorem exercise_3_21
+theorem rudin_exercise_3_21
   {X : Type*} [MetricSpace X] [CompleteSpace X]
   (E : ℕ → Set X)
   (hE : ∀ n, E n ⊃ E (n + 1))
@@ -199,16 +199,16 @@ theorem exercise_3_21
   ∃ a, Set.iInter E = {a} :=
 sorry
 
-theorem exercise_3_22 (X : Type*) [MetricSpace X] [CompleteSpace X]
+theorem rudin_exercise_3_22 (X : Type*) [MetricSpace X] [CompleteSpace X]
   (G : ℕ → Set X) (hG : ∀ n, IsOpen (G n) ∧ Dense (G n)) :
   ∃ x, ∀ n, x ∈ G n :=
 sorry
 
-theorem exercise_4_1a
+theorem rudin_exercise_4_1a
   : ∃ (f : ℝ → ℝ), (∀ (x : ℝ), Tendsto (λ y => f (x + y) - f (x - y)) (𝓝 0) (𝓝 0)) ∧ ¬ Continuous f :=
 sorry
 
-theorem exercise_4_2a
+theorem rudin_exercise_4_2a
   {α : Type} [MetricSpace α]
   {β : Type} [MetricSpace β]
   (f : α → β)
@@ -216,13 +216,13 @@ theorem exercise_4_2a
   : ∀ (x : Set α), f '' (closure x) ⊆ closure (f '' x) :=
 sorry
 
-theorem exercise_4_3
+theorem rudin_exercise_4_3
   {α : Type} [MetricSpace α]
   (f : α → ℝ) (h : Continuous f) (z : Set α) (g : z = f⁻¹' {0})
   : IsClosed z :=
 sorry
 
-theorem exercise_4_4a
+theorem rudin_exercise_4_4a
   {α : Type} [MetricSpace α]
   {β : Type} [MetricSpace β]
   (f : α → β)
@@ -232,7 +232,7 @@ theorem exercise_4_4a
   : f '' Set.univ ⊆ closure (f '' s) :=
 sorry
 
-theorem exercise_4_4b
+theorem rudin_exercise_4_4b
   {α : Type} [MetricSpace α]
   {β : Type} [MetricSpace β]
   (f g : α → β)
@@ -244,7 +244,7 @@ theorem exercise_4_4b
   : f = g :=
 sorry
 
-theorem exercise_4_5a
+theorem rudin_exercise_4_5a
   (f : ℝ → ℝ)
   (E : Set ℝ)
   (h₁ : IsClosed E)
@@ -252,12 +252,12 @@ theorem exercise_4_5a
   : ∃ (g : ℝ → ℝ), Continuous g ∧ ∀ x ∈ E, f x = g x :=
 sorry
 
-theorem exercise_4_5b
+theorem rudin_exercise_4_5b
   : ∃ (E : Set ℝ) (f : ℝ → ℝ), (ContinuousOn f E) ∧
   (¬ ∃ (g : ℝ → ℝ), Continuous g ∧ ∀ x ∈ E, f x = g x) :=
 sorry
 
-theorem exercise_4_6
+theorem rudin_exercise_4_6
   (f : ℝ → ℝ)
   (E : Set ℝ)
   (G : Set (ℝ × ℝ))
@@ -266,17 +266,17 @@ theorem exercise_4_6
   : ContinuousOn f E ↔ IsCompact G :=
 sorry
 
-theorem exercise_4_8a
+theorem rudin_exercise_4_8a
   (E : Set ℝ) (f : ℝ → ℝ) (hf : UniformContinuousOn f E)
   (hE : Bornology.IsBounded E) : Bornology.IsBounded (Set.image f E) :=
 sorry
 
-theorem exercise_4_8b
+theorem rudin_exercise_4_8b
   (E : Set ℝ) :
   ∃ f : ℝ → ℝ, UniformContinuousOn f E ∧ ¬ Bornology.IsBounded (Set.image f E) :=
 sorry
 
-theorem exercise_4_11a
+theorem rudin_exercise_4_11a
   {X : Type*} [MetricSpace X]
   {Y : Type*} [MetricSpace Y]
   (f : X → Y) (hf : UniformContinuous f)
@@ -284,40 +284,40 @@ theorem exercise_4_11a
   CauchySeq (λ n => f (x n)) :=
 sorry
 
-theorem exercise_4_12
+theorem rudin_exercise_4_12
   {α β γ : Type*} [UniformSpace α] [UniformSpace β] [UniformSpace γ]
   {f : α → β} {g : β → γ}
   (hf : UniformContinuous f) (hg : UniformContinuous g) :
   UniformContinuous (g ∘ f) :=
 sorry
 
-theorem exercise_4_15 {f : ℝ → ℝ}
+theorem rudin_exercise_4_15 {f : ℝ → ℝ}
   (hf : Continuous f) (hof : IsOpenMap f) :
   Monotone f :=
 sorry
 
-theorem exercise_4_19
+theorem rudin_exercise_4_19
   {f : ℝ → ℝ} (hf : ∀ a b c, a < b → f a < c → c < f b → ∃ x, a < x ∧ x < b ∧ f x = c)
   (hg : ∀ r : ℚ, IsClosed {x | f x = r}) : Continuous f :=
 sorry
 
-theorem exercise_4_21a {X : Type*} [MetricSpace X]
+theorem rudin_exercise_4_21a {X : Type*} [MetricSpace X]
   (K F : Set X) (hK : IsCompact K) (hF : IsClosed F) (hKF : Disjoint K F) :
   ∃ (δ : ℝ), δ > 0 ∧ ∀ (p q : X), p ∈ K → q ∈ F → dist p q ≥ δ :=
 sorry
 
-theorem exercise_4_24 {f : ℝ → ℝ}
+theorem rudin_exercise_4_24 {f : ℝ → ℝ}
   (hf : Continuous f) (a b : ℝ) (hab : a < b)
   (h : ∀ x y : ℝ, a < x → x < b → a < y → y < b → f ((x + y) / 2) ≤ (f x + f y) / 2) :
   ConvexOn ℝ (Set.Ioo a b) f :=
 sorry
 
-theorem exercise_5_1
+theorem rudin_exercise_5_1
   {f : ℝ → ℝ} (hf : ∀ x y : ℝ, |(f x - f y)| ≤ (x - y) ^ 2) :
   ∃ c, f = λ x => c :=
 sorry
 
-theorem exercise_5_2 {a b : ℝ}
+theorem rudin_exercise_5_2 {a b : ℝ}
   {f g : ℝ → ℝ} (hf : ∀ x ∈ Set.Ioo a b, deriv f x > 0)
   (hg : g = f⁻¹)
   (hg_diff : DifferentiableOn ℝ g (Set.Ioo a b)) :
@@ -325,25 +325,25 @@ theorem exercise_5_2 {a b : ℝ}
   ∀ x ∈ Set.Ioo a b, deriv g x = 1 / deriv f x :=
 sorry
 
-theorem exercise_5_3 {g : ℝ → ℝ} (hg : Continuous g)
+theorem rudin_exercise_5_3 {g : ℝ → ℝ} (hg : Continuous g)
   (hg' : ∃ M : ℝ, ∀ x : ℝ, |deriv g x| ≤ M) :
   ∃ N, ∀ ε > 0, ε < N → Function.Injective (λ x : ℝ => x + ε * g x) :=
 sorry
 
-theorem exercise_5_4 {n : ℕ}
+theorem rudin_exercise_5_4 {n : ℕ}
   (C : ℕ → ℝ)
   (hC : ∑ i in (range (n + 1)), (C i) / (i + 1) = 0) :
   ∃ x, x ∈ (Set.Icc (0 : ℝ) 1) ∧ ∑ i in range (n + 1), (C i) * (x^i) = 0 :=
 sorry
 
-theorem exercise_5_5
+theorem rudin_exercise_5_5
   {f : ℝ → ℝ}
   (hfd : Differentiable ℝ f)
   (hf : Tendsto (deriv f) atTop (𝓝 0)) :
   Tendsto (λ x => f (x + 1) - f x) atTop atTop :=
 sorry
 
-theorem exercise_5_6
+theorem rudin_exercise_5_6
   {f : ℝ → ℝ}
   (hf1 : Continuous f)
   (hf2 : ∀ x, DifferentiableAt ℝ f x)
@@ -352,7 +352,7 @@ theorem exercise_5_6
   MonotoneOn (λ x => f x / x) (Set.Ioi 0) :=
 sorry
 
-theorem exercise_5_7
+theorem rudin_exercise_5_7
   {f g : ℝ → ℝ} {x : ℝ}
   (hf' : DifferentiableAt ℝ f 0)
   (hg' : DifferentiableAt ℝ g 0)
@@ -361,7 +361,7 @@ theorem exercise_5_7
   Tendsto (λ x => f x / g x) (𝓝 x) (𝓝 (deriv f x / deriv g x)) :=
 sorry
 
-theorem exercise_5_15 {f : ℝ → ℝ} (a M0 M1 M2 : ℝ)
+theorem rudin_exercise_5_15 {f : ℝ → ℝ} (a M0 M1 M2 : ℝ)
   (hf' : DifferentiableOn ℝ f (Set.Ici a))
   (hf'' : DifferentiableOn ℝ (deriv f) (Set.Ici a))
   (hM0 : M0 = sSup {(|f x|) | x ∈ (Set.Ici a)})
@@ -370,7 +370,7 @@ theorem exercise_5_15 {f : ℝ → ℝ} (a M0 M1 M2 : ℝ)
   (M1 ^ 2) ≤ 4 * M0 * M2 :=
 sorry
 
-theorem exercise_5_17
+theorem rudin_exercise_5_17
   {f : ℝ → ℝ}
   (hf' : DifferentiableOn ℝ f (Set.Icc (-1) 1))
   (hf'' : DifferentiableOn ℝ (deriv f) (Set.Icc 1 1))
