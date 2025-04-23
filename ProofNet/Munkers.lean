5365b5e777832f896c47339dcda25b44fcba94aa
Marcin Wrochna <mwrochna@gmail.com>
2025-04-23 11:14:21 +0200

    Bump to 4.19.0 rc2

--- ProofNet/Munkers.lean
+++ ProofNet/Munkers.lean
@@ -1,7 +1,7 @@
 import Mathlib
 
 open Filter Set TopologicalSpace
-open scoped Topology
+open Topology
 noncomputable section
 
 theorem munkers_exercise_13_1 (X : Type*) [TopologicalSpace X] (A : Set X)
@@ -159,12 +159,12 @@ sorry
 
 theorem munkers_exercise_22_2a {X Y : Type*} [TopologicalSpace X]
   [TopologicalSpace Y] (p : X → Y) (h : Continuous p) :
-  QuotientMap p ↔ ∃ (f : Y → X), Continuous f ∧ p ∘ f = id :=
+  IsQuotientMap p ↔ ∃ (f : Y → X), Continuous f ∧ p ∘ f = id :=
 sorry
 
 theorem munkers_exercise_22_2b {X : Type*} [TopologicalSpace X]
   {A : Set X} (r : X → A) (hr : Continuous r) (h : ∀ x : A, r x = x) :
-  QuotientMap r :=
+  IsQuotientMap r :=
 sorry
 
 theorem munkers_exercise_22_5 {X Y : Type*} [TopologicalSpace X]
@@ -212,7 +212,7 @@ theorem munkers_exercise_23_9 {X Y : Type*}
 sorry
 
 theorem munkers_exercise_23_11 {X Y : Type*} [TopologicalSpace X] [TopologicalSpace Y]
-  (p : X → Y) (hq : QuotientMap p)
+  (p : X → Y) (hq : IsQuotientMap p)
   (hY : ConnectedSpace Y) (hX : ∀ y : Y, IsConnected (p ⁻¹' {y})) :
   ConnectedSpace X :=
 sorry
@@ -232,8 +232,8 @@ theorem munkers_exercise_25_4 {X : Type*} [TopologicalSpace X]
 sorry
 
 theorem munkers_exercise_25_9 {G : Type*} [TopologicalSpace G] [Group G]
-  [TopologicalGroup G] (C : Set G) (h : C = connectedComponent 1) :
-  IsNormalSubgroup C :=
+  [IsTopologicalGroup G] (C : Set G) (h : C = connectedComponent 1) :
+  ∃ C' : Subgroup G, C' = C ∧ C'.Normal :=
 sorry
 
 theorem munkers_exercise_26_11
--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # ProofNet-lean4
-This is a Lean 4.10 version of the ProofNet dataset, originally https://github.com/zhangir-azerbayev/ProofNet.
+This is a Lean 4.19.0-rc2 version of the ProofNet dataset, originally https://github.com/zhangir-azerbayev/ProofNet.
 
 The original dataset (Lean 3) was released and maintained by Zhangir Azerbayev, Bartosz Piotrowski,  Hailey Schoelkopf, Edward W. Ayers, Dragomir Radev, and Jeremy Avigad (*ProofNet: Autoformalizing and Formally Proving Undergraduate-Level Mathematics*, [arxiv:2302.12433](https://arxiv.org/abs/2302.12433)), under an MIT license.
 
-This repo is based on the port to Lean 4.6 by [Rahul Vishwakarma](https://github.com/rahul3613/ProofNet-lean4)'s and the [technical refactoring](https://github.com/njuyxw/Proofnet-lean4/compare/60efffb605ee07bf723db4fb8058129a7c8a89bb...main) by njuyxw (Xiao-Wen Yang 杨骁文), both also MIT-licensed.
+This repo is based on the port to Lean 4.6 by [Rahul Vishwakarma](https://github.com/rahul3613/ProofNet-lean4)'s (now updated to 4.18.0) and the [technical refactoring](https://github.com/njuyxw/Proofnet-lean4/compare/60efffb605ee07bf723db4fb8058129a7c8a89bb...main) by njuyxw (Xiao-Wen Yang 杨骁文), both also MIT-licensed.
 
 This repo also fixes the formalization of putnam_exercise_2001_a5 (`∃! a : ℕ, ∃! n : ℕ` vs `∃! p : ℕ × ℕ`).
+(Now also fixed in Rahul's version).
